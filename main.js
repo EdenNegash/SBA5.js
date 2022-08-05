bbd5154d38692635e1f7fb49be12222599a77c37
Eden Negash <edensweet0330.gmil.com>
2022-08-05 12:09:24 -0400

    done

---
+++ main.js
@@ -0,0 +1,32 @@
+const first= document.getElementById('first');
+const last=document.getElementById('last');
+const age=document.getElementById('age');
+const mes=document.getElementById('mes');
+const btn =document.getElementById('btn');
+const newbtn =document.getElementById('nbtn');
+class Person {
+    constructor(){
+        this.firstName = first.value;
+        this.lastName =last.value;
+        this.age=age.value;
+    }
+}
+
+const func = ()=>{
+    document.body.style.background ='bisque';
+   const Person1 = new Person();
+   console.log(Person1);
+   let str=(`Hello,${Person1.firstName} ${Person1.lastName}. You are ${Person1.age} years old.`);
+   mes.innerHTML=str;
+   
+}
+
+btn.addEventListener('click',func);
+refreshbtn.addEventListener('click',()=>{
+    document.body.style.backgroundColor='rebeccapurple';
+    first.value='';
+    last.value='';
+    age.value='';
+    mes.innerHTML='';
+
+})
